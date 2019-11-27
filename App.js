1723798c03902275f77474a022d630795278615f
Ian <ianmmah@gmail.com>
2019-11-27 01:41:07 -0800

    Working read and write to tag

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import NfcManager, {NfcEvents, Ndef} from 'react-native-nfc-manager';
+import NfcManager, {NfcEvents, Ndef, NfcTech} from 'react-native-nfc-manager';
 import {Container, Content, Button, Text} from 'native-base';
 
 export default class App extends React.Component {
@@ -34,7 +34,10 @@ export default class App extends React.Component {
         <Content>
           <Text>This is an NFC app and I'm going SQL inject your NFC tag</Text>
           <Button onPress={this._read}>
-            <Text>Test</Text>
+            <Text>Read</Text>
+          </Button>
+          <Button onPress={this._write}>
+            <Text>Write</Text>
           </Button>
         </Content>
       </Container>
@@ -47,10 +50,35 @@ export default class App extends React.Component {
 
   async _read() {
     try {
-      await NfcManager.registerTagEvent();
+      await NfcManager.registerTagEvent(tag => {
+        NfcManager.unregisterTagEvent().catch(() => 0);
+        console.log('done');
+      });
     } catch (ex) {
       console.warn('ex', ex);
       NfcManager.unregisterTagEvent().catch(() => 0);
+    }
+  }
+
+  async _write() {
+    console.log('clicked');
+    try {
+      await NfcManager.requestTechnology(NfcTech.Ndef, {
+        alertMessage: 'Ready to write',
+      });
+      let bytes = Ndef.encodeMessage([
+        Ndef.textRecord(
+          'Hey I just met you but this is crazy but heres my number (REDACTED) so call me maybe',
+        ),
+      ]);
+      await NfcManager.writeNdefMessage(bytes);
+      console.log('successfully write ndef');
+      await NfcManager.setAlertMessageIOS('Successfully write ndef');
+    } catch (ex) {
+      await NfcManager.invalidateSessionWithErrorIOS('error writing tag!');
+      console.log('ex', ex);
+    } finally {
+      NfcManager.cancelTechnologyRequest().catch(() => 0);
     }
   }
 }
