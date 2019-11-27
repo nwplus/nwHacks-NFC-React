c99a1072c41d2d14d73d3360a7244aca2437f7a7
Ian <ianmmah@gmail.com>
2019-11-26 23:46:21 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>This is an NFC app and I'm going SQL inject your NFC tag</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
