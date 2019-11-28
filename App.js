dc39283002b25b8e8013497afe3b750ffbca401a
Ian <ianmmah@gmail.com>
2019-11-27 19:55:36 -0800

    More user instruction

--- App.js
+++ App.js
@@ -7,17 +7,36 @@
  */
 
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import NfcManager, {NfcEvents, Ndef, NfcTech} from 'react-native-nfc-manager';
-import {Container, Content, Button, Text} from 'native-base';
+import {Container, Content, Button, Text, H1} from 'native-base';
+
+const styles = StyleSheet.create({
+  content: {
+    margin: 20,
+  },
+  button: {
+    margin: 10,
+  },
+});
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
   componentDidMount() {
     NfcManager.start();
     NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
       console.log('Read a tag with payload(s):');
-      tag.ndefMessage.map(message =>
-        console.log(Ndef.text.decodePayload(message.payload)),
-      );
+      tag.ndefMessage.map(message => {
+        const payload = Ndef.text.decodePayload(message.payload);
+        console.log(payload);
+        this.setState({text: `Payload: ${payload}`});
+      });
       NfcManager.setAlertMessageIOS('I got your tag!');
       NfcManager.unregisterTagEvent().catch(() => 0);
     });
@@ -31,14 +50,15 @@ export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <Content>
+        <Content style={styles.content}>
           <Text>This is an NFC app and I'm going SQL inject your NFC tag</Text>
-          <Button onPress={this._read}>
+          <Button style={styles.button} onPress={this._read}>
             <Text>Read</Text>
           </Button>
-          <Button onPress={this._write}>
+          <Button style={styles.button} onPress={this._write}>
             <Text>Write</Text>
           </Button>
+          <H1>{this.state.text}</H1>
         </Content>
       </Container>
     );
@@ -48,37 +68,39 @@ export default class App extends React.Component {
     NfcManager.cancelTechnologyRequest().catch(() => 0);
   }
 
-  async _read() {
+  _read = async () => {
     try {
+      this.setState({text: 'Ready to read'});
       await NfcManager.registerTagEvent(tag => {
         NfcManager.unregisterTagEvent().catch(() => 0);
-        console.log('done');
       });
     } catch (ex) {
       console.warn('ex', ex);
       NfcManager.unregisterTagEvent().catch(() => 0);
     }
-  }
+  };
 
-  async _write() {
-    console.log('clicked');
+  _write = async () => {
+    this.setState({text: 'Ready to write'});
     try {
       await NfcManager.requestTechnology(NfcTech.Ndef, {
         alertMessage: 'Ready to write',
       });
       let bytes = Ndef.encodeMessage([
         Ndef.textRecord(
-          'Hey I just met you but this is crazy but heres my number (REDACTED) so call me maybe',
+          'Hey I just met you and this is crazy but heres my number (REDACTED) so call me maybe',
         ),
       ]);
       await NfcManager.writeNdefMessage(bytes);
       console.log('successfully write ndef');
+      this.setState({text: 'Successful write'});
       await NfcManager.setAlertMessageIOS('Successfully write ndef');
     } catch (ex) {
+      this.setState({text: 'wtf'});
       await NfcManager.invalidateSessionWithErrorIOS('error writing tag!');
       console.log('ex', ex);
     } finally {
       NfcManager.cancelTechnologyRequest().catch(() => 0);
     }
-  }
+  };
 }
