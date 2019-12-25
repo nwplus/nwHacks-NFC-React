6eed0ef1efa399abbb222df6fc76a949c4bfa043
Stanford Lin <stanlin1999@gmail.com>
2019-12-25 02:50:45 -0800

    refactored to functional hook

--- src/components/MenuDrawer.js
+++ src/components/MenuDrawer.js
@@ -10,44 +10,6 @@ import {
 import { View } from 'native-base';
 import { useStoreActions } from 'easy-peasy';
 
-const WIDTH = Dimensions.get('window').width
-const HEIGHT = Dimensions.get('window').height
-// const logout = useStoreActions(actions => actions.auth.logout);
-
-export default class MenuDrawer extends React.Component {
-
-    navLink(nav, text) {
-        return (
-            <TouchableOpacity style={{ height: 80 }} onPress={() => this.props.navigation.navigate(nav)}>
-                <Text style={styles.link}>{text}</Text>
-            </TouchableOpacity>
-        )
-    }
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.imgView} >
-                    <Image style={styles.img} source={require('../../nwplus_logo.png')} />
-                </View>
-                <View >
-                    {this.navLink("Home", "Home")}
-                    {this.navLink("Events", "Events")}
-                    {this.navLink("Workshops", "Workshops")}
-                    {this.navLink("Applicants", "Applicants")}
-                    {this.navLink("Coat Check", "Coat Check")}
-                    {/* {this.navLink("Scan", "Scan")} */}
-                    <TouchableOpacity style={{ height: 80 }} onPress={() => this.props.navigation.navigate("Scan")}>
-                        <Text style={styles.linkScan}>Scan</Text>
-                    </TouchableOpacity>
-                    {/* <TouchableOpacity style={{ height: 80 }} onPress={logout}>
-                        <Text style={styles.linkScan}>Logout</Text>
-                    </TouchableOpacity> */}
-                </View>
-            </View>
-        )
-    }
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -88,5 +50,54 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         paddingTop: 10,
         paddingBottom: 450,
+    },
+    linkLogout: {
+        flex: 1,
+        fontSize: 25,
+        padding: 6,
+        paddingLeft: 14,
+        margin: 5,
+        textAlign: 'center',
+        opacity: 0.5, 
+        color: 'white',
+        fontWeight: 'bold',
+        paddingTop: 40,
+        paddingBottom: 450,
     }
 })
+
+const MenuDrawer = props => {
+    const logout = useStoreActions(actions => actions.auth.logout);
+    function navLink(nav, text) {
+        return(
+            <TouchableOpacity style={{ height: 80 }} onPress={() => props.navigation.navigate(nav)}>
+                <Text style={styles.link}>{text}</Text>
+            </TouchableOpacity>
+        )
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imgView} >
+                <Image style={styles.img} source={require('../../nwplus_logo.png')} />
+            </View>
+            <View >
+                {navLink("Home", "Home")}
+                {navLink("Events", "Events")}
+                {navLink("Workshops", "Workshops")}
+                {navLink("Applicants", "Applicants")}
+                {navLink("Coat Check", "Coat Check")}
+                <TouchableOpacity style={{ height: 80 }} onPress={() => props.navigation.navigate("Scan")}>
+                    <Text style={styles.linkScan}>Scan</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ height: 80 }} onPress={logout}>
+                    <Text style={styles.linkLogout}>Logout</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+    
+
+};
+
+export default MenuDrawer;
