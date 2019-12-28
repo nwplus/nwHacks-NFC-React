4cd6072301601e383a897b804a8021e13ef4cf33
Giulio Rossi <ciuffi9@mac.com>
2019-12-27 18:28:14 -0800

    add ability to change color of menuButton

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 const MenuButton = props => {
-    return (
-        <Icon
-            name="menu"
-            size={30}
-            onPress={() => props.navigation.toggleDrawer()}
-            style={styles.menuIcon} />
-    );
-}
+  const light = props.light ? styles.light : null;
+  return (
+    <Icon
+      name="menu"
+      size={30}
+      onPress={() => props.navigation.toggleDrawer()}
+      style={[styles.menuIcon, light]}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    menuIcon: {
-        zIndex: 9,
-        position: 'absolute',
-        top: 20,
-        left: 20
-    }
-})
+  menuIcon: {
+    zIndex: 9,
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+  light: {
+    color: 'white',
+  },
+});
 
 export default MenuButton;
