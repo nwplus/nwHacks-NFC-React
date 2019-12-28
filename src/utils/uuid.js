b5de106aedd8c9501d7bc0ff3fda296efb229219
Ian <ianmmah@gmail.com>
2019-12-27 17:54:55 -0800

    Remove unused uuid

--- src/utils/uuid.js
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-import uuid from 'uuid';
-export default () => {
-  const [currUuid, setUuid] = useState('');
-  const _generateUUID = () => {
-    setUuid(uuid.v4());
-  };
-  return [currUuid, _generateUUID];
-};
