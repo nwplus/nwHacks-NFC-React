c65a108090b012da06c554997d4239025adadfd5
Giulio Rossi <ciuffi9@mac.com>
2019-12-27 18:32:34 -0800

    remove extra code

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -3,25 +3,24 @@ import {StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 const MenuButton = props => {
-  const light = props.light ? styles.light : null;
   return (
     <Icon
       name="menu"
       size={30}
       onPress={() => props.navigation.toggleDrawer()}
-      style={[styles.menuIcon, light]}
+      style={[styles.menuIcon]}
     />
   );
 };
 
 const styles = StyleSheet.create({
-    menuIcon: {
-        color: '#ffffff',
-        zIndex: 9,
-        position: 'absolute',
-        top: 20,
-        left: 20
-    }
-})
+  menuIcon: {
+    color: '#ffffff',
+    zIndex: 9,
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+});
 
 export default MenuButton;
