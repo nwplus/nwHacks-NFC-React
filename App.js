4d50bf7212f1968e13f663fedd20fd803317a497
Giulio Rossi <ciuffi9@mac.com>
2019-12-30 03:21:46 -0800

    Move router logic

--- App.js
+++ App.js
@@ -10,70 +10,10 @@ import React from 'react';
 import {Root} from 'native-base';
 import store from './src/utils/store';
 import {StoreProvider, useStoreActions} from 'easy-peasy';
-import Main from './src/views/Main';
-import Login from './src/views/Login';
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
-import {Dimensions} from 'react-native';
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
-// Screen imports
-import ScanScreen from './src/views/Scan';
-import EventsScreen from './src/views/Events';
-import WorkshopsScreen from './src/views/Workshops';
-import ApplicantsScreen from './src/views/Applicants';
-import CoatCheckScreen from './src/views/CoatCheck';
-import MenuDrawer from './src/components/MenuDrawer';
-import AttendeeScreen from './src/views/Attendee';
 
-const WIDTH = Dimensions.get('window').width;
-
-const DrawerConfig = {
-  drawerWidth: WIDTH * 0.83,
-  contentComponent: ({navigation}) => {
-    return <MenuDrawer navigation={navigation} />;
-  },
-};
-
-//Router for the app
-const DrawerNavigator = createDrawerNavigator(
-  {
-    Home: Main,
-    Attendee: AttendeeScreen,
-    Events: EventsScreen,
-    Workshops: WorkshopsScreen,
-    Applicants: ApplicantsScreen,
-    'Coat Check': CoatCheckScreen,
-    Scan: ScanScreen,
-    // put attendee back
-  },
-  DrawerConfig,
-  {
-    initialRouteName: 'Attendee',
-  },
-);
-
-const Auth = createStackNavigator(
-  {
-    Login,
-  },
-  {
-    initialRouteName: 'Login',
-  },
-);
-
-const AppNavigator = createSwitchNavigator(
-  {
-    Auth,
-    Main: DrawerNavigator,
-  },
-  {
-    initialRouteName: 'Main',
-  },
-);
-
-const App = createAppContainer(AppNavigator);
+import App from './src/Router';
 
 const persistor = persistStore(store);
 
---
+++ src/Router.js
@@ -0,0 +1,62 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {Dimensions} from 'react-native';
+import React from 'react';
+// Screen imports
+import ScanScreen from './views/Scan';
+import EventsScreen from './views/Events';
+import WorkshopsScreen from './views/Workshops';
+import ApplicantsScreen from './views/Applicants';
+import CoatCheckScreen from './views/CoatCheck';
+import MenuDrawer from './components/MenuDrawer';
+import AttendeeScreen from './views/Attendee';
+import Main from './views/Main';
+import Login from './views/Login';
+
+const WIDTH = Dimensions.get('window').width;
+
+const DrawerConfig = {
+  drawerWidth: WIDTH * 0.83,
+  contentComponent: ({navigation}) => {
+    return <MenuDrawer navigation={navigation} />;
+  },
+};
+
+//Router for the app
+const DrawerNavigator = createDrawerNavigator(
+  {
+    Home: Main,
+    Attendee: AttendeeScreen,
+    Events: EventsScreen,
+    Workshops: WorkshopsScreen,
+    Applicants: ApplicantsScreen,
+    'Coat Check': CoatCheckScreen,
+    Scan: ScanScreen,
+    // put attendee back
+  },
+  DrawerConfig,
+);
+
+const Auth = createStackNavigator(
+  {
+    Login,
+  },
+  {
+    initialRouteName: 'Login',
+  },
+);
+
+const AppNavigator = createSwitchNavigator(
+  {
+    Auth,
+    Main: DrawerNavigator,
+  },
+  {
+    initialRouteName: 'Main',
+  },
+);
+
+const App = createAppContainer(AppNavigator);
+
+export default App;
