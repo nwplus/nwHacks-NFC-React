3d96a4661acfc54e60ac46d2113e0d8addfa68fc
Giulio Rossi <ciuffi9@mac.com>
2020-01-01 18:20:11 -0800

    add applicant view

--- src/views/Applicants.js
+++ src/views/Applicants.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MenuButton from '../components/MenuButton';
-
+import React, {useState, useEffect} from 'react';
+import {Text} from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Button,
+  Icon,
+  Body,
+  Title,
+  Right,
+  Content,
+  List,
+  ListItem,
+  Item,
+  Input,
+} from 'native-base';
+import {useStoreState} from 'easy-peasy';
 const ApplicantsScreen = props => {
-    return (
-        <View style={styles.container}>
-            <MenuButton navigation={props.navigation} />
-            <Text style={styles.text}>Applicants</Text>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 30,
-    }
-});
+  const [search, setSearch] = useState('');
+  const hackers = useStoreState(state => state.hackers.items);
+  const filteredHackers =
+    search === ''
+      ? hackers
+      : hackers.filter(
+          hacker =>
+            (hacker.firstname &&
+              hacker.firstname.toLowerCase().includes(search.toLowerCase())) ||
+            (hacker.lastname &&
+              hacker.lastname.toLowerCase().includes(search.toLowerCase())),
+        );
+  return (
+    <Container>
+      <Header style={{marginBottom: 0}}>
+        <Left>
+          <Button onPress={() => props.navigation.navigate('Home')} transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={{width: 200}}>Hackers</Title>
+        </Body>
+        <Right />
+      </Header>
+      <Item style={{marginLeft: 20}}>
+        <Icon name="ios-search" />
+        <Input value={search} onChangeText={setSearch} placeholder="Search" />
+      </Item>
+      <Content>
+        <List>
+          {filteredHackers.map((hacker, i) => {
+            return (
+              <ListItem key={hacker.email} onPress={() => {}}>
+                <Text>
+                  {!!hacker.firstname && hacker.firstname}{' '}
+                  {!!hacker.lastname && hacker.lastname}
+                </Text>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Content>
+    </Container>
+  );
+};
 
 export default ApplicantsScreen;
